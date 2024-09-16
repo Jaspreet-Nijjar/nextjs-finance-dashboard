d4bbeea1bb51552bd927473a94a84671669dff37
Jaspreet <jaspreetkn92@gmail.com>
2024-09-16 12:39:03 +0100

    added styling to footer

--- app/components/Footer.tsx
+++ app/components/Footer.tsx
@@ -1,7 +1,7 @@
 const Footer = () => {
   return (
     <footer
-      className=" bg-black text-white text-center p-2
+      className="fixed bottom-0 w-screen bg-black text-white text-center p-2
   hover:text-orange-500 transition ease-in-out duration-300 text-sm pb-4"
     >
       <p>Created By Jaspreet Nijjar</p>
