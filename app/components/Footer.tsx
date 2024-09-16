6b81f47f5e4eea8f36f705122d86608f0353955e
Jaspreet <jaspreetkn92@gmail.com>
2024-09-16 12:36:31 +0100

    created footer component

---
+++ app/components/Footer.tsx
@@ -0,0 +1,12 @@
+const Footer = () => {
+  return (
+    <footer
+      className=" bg-black text-white text-center p-2
+  hover:text-orange-500 transition ease-in-out duration-300 text-sm pb-4"
+    >
+      <p>Created By Jaspreet Nijjar</p>
+    </footer>
+  );
+};
+
+export default Footer;
