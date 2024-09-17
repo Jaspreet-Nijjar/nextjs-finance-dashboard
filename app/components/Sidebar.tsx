3406f7196387e89110ecfd66cc05d6ceea6c8678
Jaspreet <jaspreetkn92@gmail.com>
2024-09-17 09:37:39 +0100

    added styling to the sidebar

--- app/components/Sidebar.tsx
+++ app/components/Sidebar.tsx
@@ -3,13 +3,17 @@ import Link from 'next/link';
 
 const Sidebar = () => {
   return (
-    <nav>
+    <nav className="bg-black h-screen w-[20%] p-8 text-white flex flex-col text-xl gap-6">
       {SIDEBAR_LINKS.map((link) => {
         const LinkIcon = link.icon;
 
         return (
           <ul>
-            <Link key={link.label} href={link.route}>
+            <Link
+              key={link.label}
+              href={link.route}
+              className="flex gap-4 items-center mt-8"
+            >
               <LinkIcon />
               {link.label}
             </Link>
