1ecd4b0e40918887f4f62dddfe272e502693be22
Jaspreet <jaspreetkn92@gmail.com>
2024-09-18 10:42:54 +0100

    extracted the logo into it's own component

---
+++ app/components/Logo.tsx
@@ -0,0 +1,18 @@
+import Link from 'next/link';
+import { AiOutlineStock } from 'react-icons/ai';
+
+const Logo = () => {
+  return (
+    <Link href="/" className="flex items-center gap-6">
+      <AiOutlineStock size={60} className="bg-orange-500 rounded-full p-2" />
+      <div>
+        <h1 className="mt-4">
+          Quick<span className="text-orange-500">Track</span>
+        </h1>
+        <p className="text-gray-400 text-sm text-center">Finance Manager</p>
+      </div>
+    </Link>
+  );
+};
+
+export default Logo;
