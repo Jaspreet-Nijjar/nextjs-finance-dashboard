0944969b8416e268208110fe338acb70cac15684
Jaspreet <jaspreetkn92@gmail.com>
2024-09-18 10:45:34 +0100

    added logo to the sidebar

--- app/components/Sidebar.tsx
+++ app/components/Sidebar.tsx
@@ -2,23 +2,15 @@
 
 import { SIDEBAR_LINKS } from '@/constants';
 import Link from 'next/link';
-import { AiOutlineStock } from 'react-icons/ai';
 import { usePathname } from 'next/navigation';
+import Logo from './Logo';
 
 const Sidebar = () => {
   const pathname = usePathname();
 
   return (
     <nav className="sidebar p-4">
-      <Link href="/" className="flex items-center gap-6">
-        <AiOutlineStock size={60} className="bg-orange-500 rounded-full p-2" />
-        <div>
-          <h1 className="mt-4">
-            Quick<span className="text-orange-500">Track</span>
-          </h1>
-          <p className="text-gray-400 text-sm text-center">Finance Manager</p>
-        </div>
-      </Link>
+      <Logo />
 
       <ul className="space-y-4 py-8">
         {SIDEBAR_LINKS.map((link) => {
