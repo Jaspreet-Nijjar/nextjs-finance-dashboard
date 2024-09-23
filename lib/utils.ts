afa5d4dca6c39caf0750fb37b947216e6c748c0e
Jaspreet <jaspreetkn92@gmail.com>
2024-09-23 18:19:02 +0100

    added formatNumber utility function

--- lib/utils.ts
+++ lib/utils.ts
@@ -1,6 +1,12 @@
-import { clsx, type ClassValue } from "clsx"
-import { twMerge } from "tailwind-merge"
+import { clsx, type ClassValue } from 'clsx';
+import { twMerge } from 'tailwind-merge';
+import numeral from 'numeral';
 
 export function cn(...inputs: ClassValue[]) {
-  return twMerge(clsx(inputs))
+  return twMerge(clsx(inputs));
 }
+
+export const formatNumber = (value: number) => {
+  const formattedNumber = numeral(value).format('0,0');
+  return formattedNumber;
+};
