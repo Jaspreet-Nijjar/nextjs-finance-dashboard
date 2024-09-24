41d577a4777a3920f689318dc08f71be3a7283ce
Jaspreet <jaspreetkn92@gmail.com>
2024-09-24 09:07:11 +0100

    added indicator component

---
+++ app/components/common/Indicators.tsx
@@ -0,0 +1,26 @@
+import { IoMdArrowDropup, IoMdArrowDropdown } from 'react-icons/io';
+
+interface IndicatorProps {
+  data: number;
+  fontSize?: string;
+}
+
+const Indicators = ({ data, fontSize }: IndicatorProps) => {
+  return (
+    <div className={`flex items-center justify-center ${fontSize}`}>
+      {data > 0 ? (
+        <>
+          <IoMdArrowDropup size={20} className="text-green-500" />
+          <p className="text-green-500">{Math.abs(data).toFixed(1)}%</p>
+        </>
+      ) : (
+        <>
+          <IoMdArrowDropdown size={20} className="text-red-500" />
+          <p className="text-red-400">{Math.abs(data).toFixed(1)}%</p>
+        </>
+      )}
+    </div>
+  );
+};
+
+export default Indicators;
