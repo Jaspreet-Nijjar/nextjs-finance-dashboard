0a8988785a206c5192c71e1d3782419cca2e377d
Jaspreet <jaspreetkn92@gmail.com>
2024-09-24 08:27:00 +0100

    creating the tradingVolume component

--- app/components/crypto/TradingVolume/TradingVolume.tsx
+++ app/components/crypto/TradingVolume/TradingVolume.tsx
@@ -1,5 +1,15 @@
-const TradingVolume = () => {
-  return <div>Trading</div>;
+import { getTradingVolume } from '@/actions/crypto';
+
+const TradingVolume = async () => {
+  const tradingVolumeData = await getTradingVolume();
+  const tradingVolume = tradingVolumeData.usd;
+
+  return (
+    <div className="mt-4 border p-4 border-gray-200 rounded w-72">
+      <h1 className="text-xl font-bold tracking-wide">${tradingVolume}</h1>
+      <p className="text-gray-500 text-md">24hr Trading Volume</p>
+    </div>
+  );
 };
 
 export default TradingVolume;
