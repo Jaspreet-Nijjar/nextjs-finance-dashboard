07af8707304fc93a782ea5e4c4fbbde5f37f0df4
Jaspreet <jaspreetkn92@gmail.com>
2024-09-24 08:57:07 +0100

    created the MarketCap component

--- app/components/crypto/MarketCap/MarketCap.tsx
+++ app/components/crypto/MarketCap/MarketCap.tsx
@@ -1,5 +1,23 @@
-const MarketCap = () => {
-  return <div>Market Cap</div>;
+import { getGlobalData } from '@/actions/crypto';
+import { Card, CardContent, CardFooter } from '../../ui/card';
+import { cn, formatNumber } from '@/lib/utils';
+
+const MarketCap = async () => {
+  const { totalMarketCap } = await getGlobalData();
+  const marketCap = totalMarketCap.usd;
+
+  return (
+    <Card className={cn('w-fit')}>
+      <CardContent className={cn('p-2')}>
+        <h1 className="text-xl font-bold text-left">
+          ${formatNumber(marketCap)}
+        </h1>
+      </CardContent>
+      <CardFooter className={cn('px-2')}>
+        <p className="text-gray-500 text-[15px]">Market Capitalisation</p>
+      </CardFooter>
+    </Card>
+  );
 };
 
 export default MarketCap;
--- app/components/crypto/TradingVolume/TradingVolume.tsx
+++ app/components/crypto/TradingVolume/TradingVolume.tsx
@@ -1,19 +1,19 @@
-import { getTradingVolume } from '@/actions/crypto';
+import { getGlobalData } from '@/actions/crypto';
 import { Card, CardContent, CardFooter } from '../../ui/card';
 import { cn, formatNumber } from '@/lib/utils';
 
 const TradingVolume = async () => {
-  const tradingVolumeData = await getTradingVolume();
-  const tradingVolume = tradingVolumeData.usd;
+  const { totalVolume } = await getGlobalData();
+  const tradingVolume = totalVolume.usd;
 
   return (
-    <Card className={cn('w-fit')}>
-      <CardContent className={cn('p-4')}>
+    <Card className={cn('w-full')}>
+      <CardContent className={cn('p-2')}>
         <h1 className="text-xl font-bold text-left">
           ${formatNumber(tradingVolume)}
         </h1>
       </CardContent>
-      <CardFooter className={cn('px-4')}>
+      <CardFooter className={cn('px-2')}>
         <p className="text-gray-500 text-[15px]">24hr Trading Volume</p>
       </CardFooter>
     </Card>
