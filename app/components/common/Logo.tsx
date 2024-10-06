bd3741d07554da1c51cc489b5ae4485ac46ee0e1
Jaspreet <jaspreetkn92@gmail.com>
2024-10-06 13:28:01 +0100

    revised the logo styling to be responsive

--- app/components/common/Logo.tsx
+++ app/components/common/Logo.tsx
@@ -3,9 +3,9 @@ import { AiOutlineStock } from 'react-icons/ai';
 
 const Logo = () => {
   return (
-    <Link href="/" className="flex items-center gap-6 mt-8">
-      <AiOutlineStock size={60} className="bg-orange-500 rounded-full p-2" />
-      <div>
+    <Link href="/" className="flex flex-col items-center gap-6 mt-12">
+      <AiOutlineStock size={45} className="bg-orange-500 rounded-full p-2" />
+      <div className="hidden md:block">
         <h1 className="mt-4 text-3xl text-center">
           Quick<span className="text-orange-500">Track</span>
         </h1>
