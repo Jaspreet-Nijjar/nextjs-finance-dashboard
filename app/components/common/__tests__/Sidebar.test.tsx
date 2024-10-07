810707e60b2dbee23690fc8492295571b306c07a
Jaspreet <jaspreetkn92@gmail.com>
2024-10-07 11:49:22 +0100

    added second test to sidebar

--- app/components/common/__tests__/Sidebar.test.tsx
+++ app/components/common/__tests__/Sidebar.test.tsx
@@ -19,4 +19,25 @@ describe('Sidebar', () => {
     expect(screen.getByText('Transactions')).toBeInTheDocument();
     expect(screen.getByText('News')).toBeInTheDocument();
   });
+
+  it('contains the correct links with href attributes', () => {
+    render(<Sidebar />);
+    expect(screen.getByText('Dashboard').closest('a')).toHaveAttribute(
+      'href',
+      '/'
+    );
+    expect(screen.getByText('Markets').closest('a')).toHaveAttribute(
+      'href',
+      '/markets'
+    );
+
+    expect(screen.getByText('Transactions').closest('a')).toHaveAttribute(
+      'href',
+      '/transactions'
+    );
+    expect(screen.getByText('News').closest('a')).toHaveAttribute(
+      'href',
+      '/news'
+    );
+  });
 });
