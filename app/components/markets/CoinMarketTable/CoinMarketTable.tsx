d37f574341d3dc2646c98490aa8acb5ca5590b94
Jaspreet <jaspreetkn92@gmail.com>
2024-10-09 14:15:01 +0100

    added CoinProps

--- app/components/markets/CoinMarketTable/CoinMarketTable.tsx
+++ app/components/markets/CoinMarketTable/CoinMarketTable.tsx
@@ -1,6 +1,11 @@
+'use client';
+import { useEffect, useState } from 'react';
 import CoinHeaders from './CoinHeaders';
+import { CoinProps } from '@/types/crypto';
 
 const CoinMarketTable = () => {
+  const [coins, setCoins] = useState<CoinProps[]>([]);
+
   return (
     <div>
       <CoinHeaders />
