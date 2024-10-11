c277ac993052ac7b0f8fb3aa91e11b53d4c39acb
Jaspreet <jaspreetkn92@gmail.com>
2024-10-11 13:45:03 +0100

    creating the TrendingCategories component

---
+++ app/components/markets/TrendingCategories/TrendingCategories.tsx
@@ -0,0 +1,31 @@
+import { getTrendingData } from '@/actions/crypto';
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardHeader,
+  CardTitle,
+} from '../../ui/card';
+import { cn } from '@/lib/utils';
+import TrendingCategoriesList from './TrendingCategoriesList';
+
+const TrendingCategories = async () => {
+  const trendingData = await getTrendingData();
+  const trendingCategories = trendingData.categories.slice(0, 3);
+
+  return (
+    <Card className={cn('w-[90%] lg:w-fit hidden xl:block')}>
+      <CardHeader>
+        <CardTitle className="text-center">Trending Categories</CardTitle>
+        <CardDescription>
+          These are the top 3 trending categories in the last 24 hours
+        </CardDescription>
+      </CardHeader>
+      <CardContent>
+        <TrendingCategoriesList categories={trendingCategories} />
+      </CardContent>
+    </Card>
+  );
+};
+
+export default TrendingCategories;
