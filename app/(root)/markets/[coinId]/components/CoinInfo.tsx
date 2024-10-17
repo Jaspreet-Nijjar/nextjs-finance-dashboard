9a420a505845f832d45b33a328dfbb37812f6a3e
Jaspreet <jaspreetkn92@gmail.com>
2024-10-17 09:45:38 +0100

    installed clsx

--- app/(root)/markets/[coinId]/components/CoinInfo.tsx
+++ app/(root)/markets/[coinId]/components/CoinInfo.tsx
@@ -1,11 +1,12 @@
 import Indicators from '@/app/components/common/Indicators';
+import { formatNumber } from '@/lib/utils';
 import Image from 'next/image';
 
-const CoinInfo = ({ coin, marketData }) => {
+const CoinInfo = ({ coin }) => {
   return (
     <section>
       <div className="flex items-center gap-2">
-        <Image src={coin.image.large} alt={coin.name} width={40} height={100} />
+        <Image src={coin.image} alt={coin.name} width={40} height={100} />
         <h3 className="font-bold text-[18px]">{coin.name}</h3>
         <p className="uppercase text-xs text-gray-600">{coin.symbol}</p>
         <div className="bg-gray-300 w-8 text-center rounded-xl text-xs">
@@ -13,9 +14,11 @@ const CoinInfo = ({ coin, marketData }) => {
         </div>
       </div>
 
-      <div>
-        <h1>{marketData.current_price}</h1>
-        <Indicators data={marketData.price_change_percentage_24h} />
+      <div className="flex gap-2 mt-2">
+        <h1 className="text-3xl font-semibold">
+          ${formatNumber(coin.current_price)}
+        </h1>
+        <Indicators data={coin.price_change_percentage_24h} />
       </div>
     </section>
   );
--- app/(root)/markets/[coinId]/page.tsx
+++ app/(root)/markets/[coinId]/page.tsx
@@ -1,13 +1,12 @@
-import { getSingleCoin, getSingleCoinMarketData } from '@/actions/markets';
+import { getSingleCoin } from '@/actions/markets';
 import CoinInfo from './components/CoinInfo';
 
 const SingleCoin = async ({ params }: { params: { coinId: string } }) => {
   const coin = await getSingleCoin(params.coinId);
-  const coinMarketData = await getSingleCoinMarketData(params.coinId);
 
   return (
     <div className="flex flex-col p-4 mt-12 ml-[70px] md:ml-[250px]">
-      <CoinInfo coin={coin} marketData={coinMarketData} />
+      <CoinInfo coin={coin} />
     </div>
   );
 };
