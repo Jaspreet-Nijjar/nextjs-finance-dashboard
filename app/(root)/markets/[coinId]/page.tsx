414a27bda2f659f05050184081d48a076cc1357c
Jaspreet <jaspreetkn92@gmail.com>
2024-10-18 08:08:03 +0100

    created metadata for the single coin page

--- app/(root)/markets/[coinId]/page.tsx
+++ app/(root)/markets/[coinId]/page.tsx
@@ -1,6 +1,19 @@
 import { getSingleCoin } from '@/actions/markets';
 import CoinInfo from './components/CoinInfo';
 
+export async function generateMetadata({
+  params,
+}: {
+  params: { coinId: string };
+}) {
+  const coin = await getSingleCoin(params.coinId);
+
+  return {
+    title: `${coin.name}`,
+    description: `Learn more about ${coin.name}, including the price, market cap and other financial information.`,
+  };
+}
+
 const SingleCoin = async ({ params }: { params: { coinId: string } }) => {
   const coin = await getSingleCoin(params.coinId);
 
