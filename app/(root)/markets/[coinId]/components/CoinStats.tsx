c0a828c5d44c288728569d90131319e714aa92e6
Jaspreet <jaspreetkn92@gmail.com>
2024-10-20 13:54:57 +0100

    styled the CoinStats component

--- app/(root)/markets/[coinId]/components/CoinStats.tsx
+++ app/(root)/markets/[coinId]/components/CoinStats.tsx
@@ -4,35 +4,42 @@ import { formatNumber } from '@/lib/utils';
 
 const CoinStats = ({ coin }: { coin: CoinStatsProps }) => {
   return (
-    <section>
-      <CoinStatsCard
-        title="Market Cap"
-        data={coin.market_data.market_cap.usd}
-        currency={true}
-      />
-      <CoinStatsCard
-        title="Total Volume"
-        data={coin.market_data.total_volume.usd}
-        currency={true}
-      />
+    <section className="flex flex-col">
+      <div className="flex gap-2">
+        <CoinStatsCard
+          title="Market Cap"
+          data={coin.market_data.market_cap.usd}
+          currency={true}
+        />
+        <CoinStatsCard
+          title="Total Volume"
+          data={coin.market_data.total_volume.usd}
+          currency={true}
+        />
+      </div>
 
-      <CoinStatsCard
-        title="Fully Diluted Validation"
-        data={coin.market_data.fully_diluted_valuation.usd}
-        currency={true}
-      />
-      <CoinStatsCard
-        title="Circulating Supply"
-        data={formatNumber(coin.market_data.circulating_supply)}
-      />
-      <CoinStatsCard
-        title="Total Supply"
-        data={formatNumber(coin.market_data.total_supply)}
-      />
-      <CoinStatsCard
-        title="Max Supply"
-        data={formatNumber(coin.market_data.max_supply)}
-      />
+      <div className="flex gap-2 mt-2 mb-2">
+        <CoinStatsCard
+          title="Fully Diluted Validation"
+          data={coin.market_data.fully_diluted_valuation.usd}
+          currency={true}
+        />
+        <CoinStatsCard
+          title="Circulating Supply"
+          data={formatNumber(coin.market_data.circulating_supply)}
+        />
+      </div>
+
+      <div className="flex gap-2 mb-2">
+        <CoinStatsCard
+          title="Total Supply"
+          data={formatNumber(coin.market_data.total_supply)}
+        />
+        <CoinStatsCard
+          title="Max Supply"
+          data={formatNumber(coin.market_data.max_supply)}
+        />
+      </div>
     </section>
   );
 };
--- app/(root)/markets/[coinId]/components/CoinStatsCard.tsx
+++ app/(root)/markets/[coinId]/components/CoinStatsCard.tsx
@@ -1,14 +1,19 @@
 import millify from 'millify';
 import { Card, CardContent } from '../../../../components/ui/card';
+import { cn } from '@/lib/utils';
 
 const CoinStatsCard = ({ title, currency = false, data }: any) => {
   return (
-    <Card>
-      <CardContent>
-        <p>{title}</p>
-        <p>{currency ? `$${millify(data)}` : data}</p>
-      </CardContent>
-    </Card>
+    <div>
+      <Card className={cn('w-[180px]')}>
+        <CardContent>
+          <p className="text-xs text-center mt-2">{title}</p>
+          <p className="text-[15px] text-center font-semibold">
+            {currency ? `$${millify(data)}` : data}
+          </p>
+        </CardContent>
+      </Card>
+    </div>
   );
 };
 
