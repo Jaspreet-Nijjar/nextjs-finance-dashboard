e874130c7cf3162c4fcb74790949edbe2adfc257
Jaspreet <jaspreetkn92@gmail.com>
2024-10-22 15:15:37 +0100

    connected the Watchlist component to the watchlist store

--- app/(root)/portfolio/components/Watchlist.tsx
+++ app/(root)/portfolio/components/Watchlist.tsx
@@ -1,7 +1,28 @@
-import React from 'react';
+'use client';
+
+import useWatchlistStore from '@/store/watchlistStore';
+import Image from 'next/image';
 
 const Watchlist = () => {
-  return <div>Watchlist</div>;
+  const watchlist = useWatchlistStore((state) => state.watchlist);
+
+  return (
+    <div>
+      <ul>
+        {watchlist.map((coin) => (
+          <li key={coin.id}>
+            <p>{coin.name}</p>
+            <Image
+              src={coin.image.large}
+              alt={coin.name}
+              width={100}
+              height={100}
+            />
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
 };
 
 export default Watchlist;
--- app/(root)/portfolio/page.tsx
+++ app/(root)/portfolio/page.tsx
@@ -1,10 +1,17 @@
+import Watchlist from './components/Watchlist';
+
 export const metadata = {
   title: 'Portfolio',
   description: 'Explore your personal portfolio',
 };
 
 const Portfolio = () => {
-  return <div>Portfolio</div>;
+  return (
+    <div className="flex flex-col p-4 mt-12 ml-[70px] md:ml-[250px]">
+      <h1>Portfolio</h1>
+      <Watchlist />
+    </div>
+  );
 };
 
 export default Portfolio;
