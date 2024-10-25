d5329bcfb67037c76e17cb6e672ff9ad99a4156e
Jaspreet <jaspreetkn92@gmail.com>
2024-10-25 14:21:12 +0100

    added conditional rendering if no coins

--- app/components/dashboard/Balance/BalanceCard.tsx
+++ app/components/dashboard/Balance/BalanceCard.tsx
@@ -1,6 +1,7 @@
 import { formatNumber } from '@/lib/utils';
 import { Card, CardContent, CardHeader, CardTitle } from '../../ui/card';
 import BalanceDoughnutChart from './BalanceDoughnutChart';
+import Link from 'next/link';
 
 const BalanceCard = ({ totalBalance, assets }) => {
   return (
@@ -13,7 +14,21 @@ const BalanceCard = ({ totalBalance, assets }) => {
           <h1 className="mt-[-20px] text-3xl font-semibold">
             ${formatNumber(totalBalance)}
           </h1>
-          <BalanceDoughnutChart assets={assets} />
+          {assets.length > 0 ? (
+            <BalanceDoughnutChart assets={assets} />
+          ) : (
+            <div>
+              <p className="text-gray-600 text-[13px]">
+                You currently do not have any assets.
+              </p>
+              <Link
+                href="/markets"
+                className="border border-gray-300 rounded px-2 py-0.5 text-sm bg-black text-white hover:text-orange-500 transition-all"
+              >
+                View All Coins
+              </Link>
+            </div>
+          )}
         </div>
       </CardContent>
     </Card>
