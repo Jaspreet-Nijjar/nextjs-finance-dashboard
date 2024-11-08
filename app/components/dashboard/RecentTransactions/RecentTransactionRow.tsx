c859f84a85cf10584ceb2e3d2b9c51e45cca192a
Jaspreet <jaspreetkn92@gmail.com>
2024-11-08 10:50:57 +0000

    resolved error for image on RecentTransactionRow component

--- app/components/dashboard/RecentTransactions/RecentTransactionRow.tsx
+++ app/components/dashboard/RecentTransactions/RecentTransactionRow.tsx
@@ -20,12 +20,18 @@ const RecentTransactionRow = ({
             className="mb-3 text-center text-gray-600 grid grid-cols-3 text-[13px] justify-between"
           >
             <div className="flex items-center gap-4">
-              <Image
-                src={transaction.image.small}
-                alt="coin logo"
-                width={20}
-                height={20}
-              />
+              {transaction.image.small ? (
+                <Image
+                  src={transaction.image.small}
+                  alt={`${transaction.name} logo`}
+                  width={20}
+                  height={20}
+                />
+              ) : (
+                <div className="w-5 h-5 bg-gray-300 flex items-center justify-center text-gray-600">
+                  <span className="text-xs">?</span>
+                </div>
+              )}
               <Link
                 href={`/markets/${transaction.id}`}
                 className="hover:text-orange-500 transition-all"
